c265b5fef8adcaa2a9999fc137f7ba0dc6d961b8
SDSANTOSYT <129241099+SDSANTOSYT@users.noreply.github.com>
2024-11-18 22:19:19 -0500

    Inicialización de la clase que hará los ordenamientos

--- src/Worker.java
+++ src/Worker.java
@@ -1,2 +1,25 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Worker {
+
+    public void startServer(){
+        ServerSocket server = null;
+        Socket socket = null;
+        int port = 5000;
+
+        try{
+            server = new ServerSocket(port);
+            System.out.println("Server Started");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+
+    }
 }
