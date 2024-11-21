ad27db2ad2b17b63d9a21d802c8372032f6102bf
Digaes <digaes05@gmail.com>
2024-11-20 21:36:19 -0500

    heapSort

--- src/Sorter.java
+++ src/Sorter.java
@@ -47,4 +47,56 @@ public class Sorter {
 
         }
     }
+
+   //HeapSort
+   public void heapsort(int[] v){
+        int n = v.length;
+        //Contruyamos un heap :D
+        for (int i = n/2 -1; i >= 0; i--) {
+            heapify(v,n,i);
+        }
+        //Extraer elementos del heap uno por uno
+        for (int i = n-1; i >= 0; i--) {
+            //Mover la raiz actual al final
+            int temp = v[0];
+            v[0] = v[i];
+            v[i] = temp;
+
+            heapify(v,i,0);
+        }
+   }
+    //Aqui construimos el heap
+   void heapify(int[] v, int n, int i) {
+        int largest = i; //Nodo mas grande como raiz
+        int l = 2*i + 1; // izquierda
+        int r = 2*i + 2; // derecha
+
+       //Si el hijo izquierdo es mas grande que la raiz
+        if(l<n && v[l]>v[largest]){
+            largest = l;
+        }
+        //Si el hijo derecho es mas grande que el nodo mas grande hasta ahora
+        if(r<n && v[r]>v[largest]){
+            largest = r;
+        }
+
+        //Si el nodo mas grande no es la raiz
+        if(largest != i){
+            int swap = v[i];
+            v[i] = v[largest];
+            v[largest] = swap;
+
+            heapify(v,n,largest);
+        }
+   }
+
+   static void printArray(int[] v){
+        int n = v.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(v[i]+" ");
+        }
+   }
+
+   //QuickSort
+
 }
