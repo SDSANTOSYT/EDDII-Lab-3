79d84865a18179d628f0fb0727f4148fa158fd42
SDSANTOSYT <129241099+SDSANTOSYT@users.noreply.github.com>
2024-11-22 20:56:19 -0500

    Update Client.java
    se agregó el cierre del reader

--- src/Client.java
+++ src/Client.java
@@ -77,6 +77,7 @@ public class Client {
                 // Convertir la línea a número entero y agregarla a la lista
                 array.add(Integer.parseInt(linea));
             }
+            br.close();
             return array.stream().mapToInt(i -> i).toArray();
         } catch (IOException e) {
             System.out.println(e.getMessage());
